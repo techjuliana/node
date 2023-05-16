b55334f740057ffb94c4af4419abe2b721ac697f
techjuliana <julianasouzabittencourt@gmail.com>
2023-05-16 10:04:05 -0300

    projetos

--- projeto2/index.js
+++ projeto2/index.js
@@ -19,7 +19,7 @@ inquirer.prompt([
         if(resposta2.p2 >= 18){
             console.log(chalk.green(`${resposta.p1}, voce ja pode tirar sua CNH!`));
         }else{
-            console.log(chalk.bgRed(`${resposta.p1}, voce ainda tem apenas ${resposta2.p2} anos, precisa esperar mais ${18-resposta2.p2}`));
+            console.log(chalk.bgRed(`${resposta.p1}, voce ainda tem apenas ${resposta2.p2} anos, precisa esperar mais ${18-resposta2.p2} anos`));
         }
     })       
 })
