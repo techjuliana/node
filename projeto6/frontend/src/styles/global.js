6594e421ffb1b6576333874d771aba86eb9b0257
techjuliana <julianasouzabittencourt@gmail.com>
2023-08-23 02:13:55 -0300

    add projeto 6

---
+++ projeto6/frontend/src/styles/global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: 'poppins', sans-serif;
+  }
+  
+  body {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    background-image: linear-gradient(to right top, #d16ba5, #c777b9, #ba83ca, #aa8fd8, #9a9ae1, #8aa7ec, #79b3f4, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1);
+  }
+`;
+
+export default Global;
